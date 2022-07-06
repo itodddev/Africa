3e8c55142d0ad6e91c3b6ccbb3ae253ab74075b9
Todd James <todd@itodd.co>
2022-07-06 15:11:09 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Todd James on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
