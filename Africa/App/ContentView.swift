73cfde8e134c13318c2581fa01e4d7c1b9c60313
Todd James <todd@itodd.co>
2022-07-07 16:13:10 -0400

    Animal Detail View

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+  let animals: [Animal] = Bundle.main.decode("animals.json")
+
   var body: some View {
     NavigationView {
       List {
         CoverImageView()
-          .frame(height: 300)
+          .frame(height: 300) // scaleToFill() will fill the frame
           .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+
+        ForEach(animals) { animal in
+          NavigationLink(destination: AnimalDetailView(animal: animal)) {
+            AnimalListItemView(animal: animal)
+          }
+        }
       }
       .navigationBarTitle("Africa", displayMode: .large)
     }
+    .navigationViewStyle(.stack)
   }
 }
 
---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,93 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by Todd James on 7/7/22.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+
+  let animal: Animal
+
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack(alignment: .center, spacing: 20) {
+        // HERO IMAGE
+        Image(animal.image)
+          .resizable()
+          .scaledToFit()
+
+        // TITLE
+        Text(animal.name.uppercased())
+          .font(.largeTitle)
+          .fontWeight(.heavy)
+          .multilineTextAlignment(.center)
+          .padding(.vertical, 8)
+          .foregroundColor(.primary)
+          .background(
+            Color.accentColor
+              .frame(height: 6)
+              .offset(y: 24)
+          )
+
+        // HEADLINE
+        Text(animal.headline)
+          .font(.headline)
+          .multilineTextAlignment(.leading)
+          .foregroundColor(.accentColor)
+          .padding(.horizontal)
+        
+        // GALLERY
+        Group {
+          HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Winderness in Pictures")
+
+          InsetGalleryView(animal: animal)
+        }
+        .padding(.horizontal)
+
+        // FACTS
+        Group {
+          HeadingView(headingImage: "questionmark.circle", headingText: "Did you know?")
+
+          InsetFactView(animal: animal)
+        }.padding(.horizontal)
+
+        // DESCRIPTION
+        Group {
+          HeadingView(headingImage: "info.circle", headingText: "All about \(animal.name)")
+
+          Text(animal.description)
+            .multilineTextAlignment(.leading)
+            .layoutPriority(1)
+        }
+        .padding(.horizontal)
+
+        // MAP
+        Group {
+          HeadingView(headingImage: "map", headingText: "National Parks")
+
+          InsetMapView()
+        }
+        .padding(.horizontal)
+        
+        // LINK
+
+      }
+      .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+    }
+  }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+
+  static let animals: [Animal] = Bundle.main.decode("animals.json")
+
+  static var previews: some View {
+    NavigationView {
+      AnimalDetailView(animal: animals[1])
+    }
+    .previewDevice("iPhone 12 Pro")
+  }
+}
