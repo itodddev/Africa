8b80b98c1967297c37232aa6ec6eb69f1047f139
Todd James <todd@itodd.co>
2022-07-07 08:03:12 -0400

    JSON Decoder

---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Todd James on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    NavigationView {
+      List {
+        CoverImageView()
+          .frame(height: 300)
+          .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+      }
+      .navigationBarTitle("Africa", displayMode: .large)
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Todd James on 7/7/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
